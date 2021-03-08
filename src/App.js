ee786b8d11f70f4b57c263e1871370d56155f94b
zaidkhan3251 <48489044+zaidkhan3251@users.noreply.github.com>
2021-03-08 14:36:03 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Home from './Home';
+import {BrowserRouter as Router,Route, Switch } from 'react-router-dom'
+import SearchPage from './SearchPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Header/>
+      <Switch>
+        <Route path="/search"><SearchPage/></Route>
+       <Route path="/"><Home/></Route>
+      
+      
+      </Switch>
+      <Footer/>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
